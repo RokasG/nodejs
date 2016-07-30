38255f236ced22baeebfdccb48d8964401788cd4
Rokas Grincevičius <rokas.grincevicius@gmail.com>
2016-07-30 18:14:43 +0300

    Simple nodejs routing
    Simple nodejs routing

---
+++ app.js
@@ -0,0 +1,37 @@
+var http = require('http'),
+    fs   = require('fs');
+
+// Static file serving
+function serveStatic(res, path, contentType, responseCode) {
+    if(!responseCode){responseCode = 200;}
+    fs.readFile(__dirname + path, function (err, data) {
+        if(err){
+            res.writeHead(500, {'Content-type' : 'text/plain'});
+            res.end('500 - internal error');
+        }else{
+            res.writeHead(responseCode, {'Content-type' : contentType});
+            res.end(data);
+        }
+    });
+}
+
+// Routing
+http.createServer(function (req, res) {
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    switch (path){
+        case '':
+            serveStatic(res, '/public/index.html','text/html');
+            break;
+        case '/about':
+            serveStatic(res, '/public/about.html','text/html');
+            break;
+        case '/img/logo.jpg':
+            break;
+        default:
+            serveStatic(res, '/public/404.html', 'text/html', 404);
+            break;
+    }
+    
+}).listen(3000);
+
+console.log('Server is serving on localhost: 3000 ');
